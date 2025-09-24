18ad4b3253862b2b355c56a902c5cd4e5155a2d9
thakurnishu <90508814+thakurnishu@users.noreply.github.com>
2025-09-24 15:14:36 +0530

    tf fmt

--- infra/main.tf
+++ infra/main.tf
@@ -1,15 +1,15 @@
 module "s3_with_cloudfront" {
-  source             = "git::https://github.com/thakurnishu/terraform_modules.git//aws/cloudfront_s3_website?ref=v2.4.1"
-  
-  bucket_name = var.bucket_name
+  source = "git::https://github.com/thakurnishu/terraform_modules.git//aws/cloudfront_s3_website?ref=v2.4.1"
+
+  bucket_name  = var.bucket_name
   project_name = var.project_name
 
-  enable_custom_domain = true
+  enable_custom_domain  = true
   website_custom_domain = var.website_custom_domain
-  acm_region = var.acm_region
+  acm_region            = var.acm_region
 
   use_cloudfront_function = true
-  function_code = file("url-rewrite.js")
+  function_code           = file("url-rewrite.js")
 
   tags = var.tags
 }
